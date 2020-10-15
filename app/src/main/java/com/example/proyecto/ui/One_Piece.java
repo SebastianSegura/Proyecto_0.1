f2f3d6eae5ec327e5cff115d3bd3fea36c3684c9
sebastian <insel_s@hotmail.com>
2020-10-15 00:29:25 -0300

    Segundo Comit

---
+++ app/src/main/java/com/example/proyecto/ui/One_Piece.java
@@ -0,0 +1,18 @@
+package com.example.proyecto.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyecto.R;
+
+public class One_Piece extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one__piece);
+
+
+    }
+}
